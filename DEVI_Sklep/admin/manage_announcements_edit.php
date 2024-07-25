10df6a7b2ce40511848932ede26104c8772d0367
NARRQ <118217739+NARRQ@users.noreply.github.com>
2024-07-25 18:12:46 +0200

    Update manage_announcements_edit.php

--- DEVI_Sklep/admin/manage_announcements_edit.php
+++ DEVI_Sklep/admin/manage_announcements_edit.php
@@ -146,19 +146,56 @@
             <h1>Panel Administratora - Edytuj ogłoszenie</h1>
             <button><a href="admin_page.php">Panel Administratora</a></button>
             <button><a href="manage_announcements.php">Lista ogłoszeń</a></button>
-            <!-- Wyświetlanie komunikatu o dodaniu ogłoszenia -->
-            <?php if (!empty($message)) : ?>
-                <div class="message">
-                    <?php echo $message; ?>
-                </div>
-            <?php endif; ?>
-            <!-- FORMULARZ -->
-            <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="POST" enctype="multipart/form-data">
+            <!-- LOGIKA -->
+            <?php
+                require('../baza/config.php');
+                // wyswietlenie danych z bazy
+                if(isset($_GET['id']) && is_numeric($_GET['id']))
+                {
+                    $id=$_GET['id'];
+                    $query="
+                    SELECT 
+                    `producent`, 
+                    `nazwa`,
+                    `procesor`, 
+                    `procesor_sz`,
+                    `ram`,
+                    `grafika`,
+                    `dysk`,
+                    `klawiatura`,
+                    `przekatna`,
+                    `rozdzielczosc`,
+                    `matryca`,
+                    `system`,
+                    `porty`,
+                    `komunikacja`,
+                    `multimedia`,
+                    `stan`,
+                    `czas_pracy`,
+                    `zasilacz`,
+                    `opis`,
+                    `cena`,
+                    `ilosc`,
+                    `miniatura`,
+                    `miniatura_nazwa`,
+                    `czy_na_stronie`
+                    FROM `laptopy` WHERE id_laptopa=$id;    
+                    ";
+                    $result=mysqli_query($conn,$query);
+                    
+                    if($result)
+                    {
+                        $row=mysqli_fetch_assoc($result);
+                    }
+                }
+            ?>
+                <!-- FORMULARZ -->
+                <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="POST" enctype="multipart/form-data">
                 <div>
                     <label for="main_image">Miniatura:</label>
                     <input type="file" id="main_image" name="main_image" accept="image/*" onchange="previewMainImage(this);" required>
                     <div id="main-image-preview" class="image-preview-container">
-                        <!-- Placeholder for main image preview -->
+                        <img src="path/to/miniatura/<?php echo htmlspecialchars($row['miniatura_nazwa']); ?>" alt="Miniatura">
                     </div>
                 </div>
                 <div>
@@ -170,90 +207,90 @@
                 </div>
                 <div>
                     <label for="manufacturer">Producent:</label>
-                    <input type="text" id="manufacturer" name="manufacturer" required>
+                    <input type="text" id="manufacturer" name="manufacturer" value="<?php echo htmlspecialchars($row['producent']); ?>" required>
                 </div>
                 <div>
                     <label for="name">Nazwa:</label>
-                    <input type="text" id="name" name="name" required>
+                    <input type="text" id="name" name="name" value="<?php echo htmlspecialchars($row['nazwa']); ?>" required>
                 </div>
                 <div>
                     <label for="processor">Procesor:</label>
-                    <input type="text" id="processor" name="processor" required>
+                    <input type="text" id="processor" name="processor" value="<?php echo htmlspecialchars($row['procesor']); ?>" required>
                 </div>
                 <div>
                     <label for="processor_size">Procesor szczegóły:</label>
-                    <input type="text" id="processor_size" name="processor_size" required>
+                    <input type="text" id="processor_size" name="processor_size" value="<?php echo htmlspecialchars($row['procesor_sz']); ?>" required>
                 </div>
                 <div>
                     <label for="ram">Pamięć RAM:</label>
-                    <input type="text" id="ram" name="ram" required>
+                    <input type="text" id="ram" name="ram" value="<?php echo htmlspecialchars($row['ram']); ?>" required>
                 </div>
                 <div>
                     <label for="graphics">Karta graficzna:</label>
-                    <input type="text" id="graphics" name="graphics" required>
+                    <input type="text" id="graphics" name="graphics" value="<?php echo htmlspecialchars($row['grafika']); ?>" required>
                 </div>
                 <div>
                     <label for="disk">Dysk:</label>
-                    <input type="text" id="disk" name="disk" required>
+                    <input type="text" id="disk" name="disk" value="<?php echo htmlspecialchars($row['dysk']); ?>" required>
                 </div>
                 <div>
                     <label for="keyboard">Układ klawiatury:</label>
-                    <input type="text" id="keyboard" name="keyboard" required>
+                    <input type="text" id="keyboard" name="keyboard" value="<?php echo htmlspecialchars($row['klawiatura']); ?>" required>
                 </div>
                 <div>
                     <label for="screen_size">Przekątna ekranu:</label>
-                    <input type="text" id="screen_size" name="screen_size" required>
+                    <input type="text" id="screen_size" name="screen_size" value="<?php echo htmlspecialchars($row['przekatna']); ?>" required>
                 </div>
                 <div>
                     <label for="resolution">Rozdzielczość:</label>
-                    <input type="text" id="resolution" name="resolution" required>
+                    <input type="text" id="resolution" name="resolution" value="<?php echo htmlspecialchars($row['rozdzielczosc']); ?>" required>
                 </div>
                 <div>
                     <label for="matrix">Typ matrycy:</label>
-                    <input type="text" id="matrix" name="matrix" required>
+                    <input type="text" id="matrix" name="matrix" value="<?php echo htmlspecialchars($row['matryca']); ?>" required>
                 </div>
                 <div>
                     <label for="system">System operacyjny:</label>
-                    <input type="text" id="system" name="system" required>
+                    <input type="text" id="system" name="system" value="<?php echo htmlspecialchars($row['system']); ?>" required>
                 </div>
                 <div>
                     <label for="ports">Porty:</label>
-                    <input type="text" id="ports" name="ports" required>
+                    <input type="text" id="ports" name="ports" value="<?php echo htmlspecialchars($row['porty']); ?>" required>
                 </div>
                 <div>
                     <label for="communication">Komunikacja:</label>
-                    <input type="text" id="communication" name="communication" required>
+                    <input type="text" id="communication" name="communication" value="<?php echo htmlspecialchars($row['komunikacja']); ?>" required>
                 </div>
                 <div>
                     <label for="multimedia">Multimedia:</label>
-                    <input type="text" id="multimedia" name="multimedia" required>
+                    <input type="text" id="multimedia" name="multimedia" value="<?php echo htmlspecialchars($row['multimedia']); ?>" required>
                 </div>
                 <div>
                     <label for="condition">Stan wizualny:</label>
-                    <input type="text" id="condition" name="condition" required>
+                    <input type="text" id="condition" name="condition" value="<?php echo htmlspecialchars($row['stan']); ?>" required>
                 </div>
                 <div>
                     <label for="working_hours">Średni czas pracy na baterii:</label>
-                    <input type="text" id="working_hours" name="working_hours" required>
+                    <input type="text" id="working_hours" name="working_hours" value="<?php echo htmlspecialchars($row['czas_pracy']); ?>" required>
                 </div>
                 <div>
                     <label for="power_supply">Zasilacz:</label>
-                    <input type="text" id="power_supply" name="power_supply" required>
+                    <input type="text" id="power_supply" name="power_supply" value="<?php echo htmlspecialchars($row['zasilacz']); ?>" required>
                 </div>
                 <div>
                     <label for="price">Cena:</label>
-                    <input type="number" id="price" name="price" required>
+                    <input type="number" id="price" name="price" value="<?php echo htmlspecialchars($row['cena']); ?>" required>
                 </div>
                 <div>
                     <label for="quantity">Ilość na stanie:</label>
-                    <input type="number" id="quantity" name="quantity" required>
+                    <input type="number" id="quantity" name="quantity" value="<?php echo htmlspecialchars($row['ilosc']); ?>" required>
                 </div>
                 <div>
                     <label for="description">Opis:</label>
-                    <textarea id="description" name="description" required></textarea>
+                    <textarea id="description" name="description" required><?php echo htmlspecialchars($row['opis']); ?></textarea>
                 </div>
                 <button type="submit">Zapisz zmiany</button>
-                <button onclick="confirmDelete('manage_announcements_delete.php?id=<?= $row['id_laptopa'] ?>')">Usuń Ogłoszenie</button>
+                <button onclick="confirmDelete('manage_announcements_delete.php?id=<?php echo $id; ?>')">Usuń Ogłoszenie</button>
             </form>
         </div>
     </main>
